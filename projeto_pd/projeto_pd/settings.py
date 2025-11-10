9e9ccfd02b1abd873528e519145f25eb3e2e954d
devclaudioalmeida <claudio.almeida.webmarketing@gmail.com>
2025-11-10 15:39:06 -0300

    Pequena alteração
    Uma leve alteração em um comentário de linha para facilitar a minha visualização no projeto

--- projeto_pd/projeto_pd/settings.py
+++ projeto_pd/projeto_pd/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # Minha aplicação
+    # Minhas aplicações
     'learning_logs',
+    # Aplicações default do Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
